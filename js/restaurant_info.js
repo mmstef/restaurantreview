43a674aeb02721560dd1096c33784ca6d72b8c0f
Magnús Margeir Stefánsson <maggistef@gmail.com>
2018-10-02 11:07:29 +0000

    Added MapBox token to restaurant_info

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibW1zdGVmIiwiYSI6ImNqbXJrbDNjejE3djEzcXBuaHB6bDJwOXYifQ.M-h-tJtzZ6dfJKVTCfoazA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
